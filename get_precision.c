8269d31456df95831ed73d6824f151f7111a0995
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-07-22 15:55:20 -0700

    get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -1,6 +1,15 @@
 #include "main.h"
 
 /**
+ * is_digit - check digit
+ * @c: char to check
+ */
+bool is_digit(char c)
+{
+	return (c >= '0' && c <= '9');
+}
+
+/**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
@@ -21,6 +30,12 @@ int get_precision(const char *format, int *i, va_list list)
 
 	precision = 0;
 
+	if (!is_digit(format[curr_i + 1]))
+	{
+		*i = curr_i;
+		return (precision);
+	}
+
 	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
 		if (is_digit(format[curr_i]))
@@ -37,6 +52,8 @@ int get_precision(const char *format, int *i, va_list list)
 		else
 			break;
 	}
+	if (precision < 0)
+		precision = -1;
 
 	*i = curr_i - 1;
 
