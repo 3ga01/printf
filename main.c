6e20a2a9aae2af7b86438e38acfa881ea7709832
Emmanuel Mireku Omari <omariemmanuel91@gmail.com>
2023-07-24 01:09:11 -0700

    test file updated

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ int main(void)
 	int len2;
 	unsigned int ui;
 	void *addr;
+	char str[] = "Hello, World!";
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
@@ -39,6 +40,33 @@ int main(void)
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
 	_printf("Unknown:[%r]\n");
-	printf("Unknown:[%%r]\n");
+	_printf("Positive precision:[%*d]\n", 3, 1234);
+	printf("Positive precision:[%*d]\n", 3, 1234);
+	len = _printf("Precision for integer: [%.*d]\n", 5, 12345);
+	len2 = printf("Precision for integer: [%.*d]\n", 5, 12345);
+	printf("Length:[%d, %d]\n", len, len2);
+	len = _printf("Precision for negative integer: [%.*d]\n", 3, -9876);
+	len2 = printf("Precision for negative integer: [%.*d]\n", 3, -9876);
+	printf("Length:[%d, %d]\n", len, len2);
+	len = _printf("Precision for float: [%.*f]\n", 2, 1234.5678);
+	len2 = printf("Precision for float: [%.*f]\n", 2, 1234.5678);
+	printf("Length:[%d, %d]\n", len, len2);
+	len = _printf("Precision for string: [%.*s]\n", 5, str);
+	len2 = printf("Precision for string: [%.*s]\n", 5, str);
+	printf("Length:[%d, %d]\n", len, len2);
+	ui = 4294967295;
+	len = _printf("Precision for unsigned int: [%.*u]\n", 7, ui);
+	len2 = printf("Precision for unsigned int: [%.*u]\n", 7, ui);
+	printf("Length:[%d, %d]\n", len, len2);
+	ui = 4294967295;
+	len = _printf("Precision for unsigned octal: [%.*o]\n", 7, ui);
+	len2 = printf("Precision for unsigned octal: [%.*o]\n", 7, ui);
+	printf("Length:[%d, %d]\n", len, len2);
+	len = _printf("Precision for unsigned hex: [%.*x]\n", 6, ui);
+	len2 = printf("Precision for unsigned hex: [%.*x]\n", 6, ui);
+	printf("Length:[%d, %d]\n", len2, len2);
+	len = _printf("Precision for character: [%*c]\n", 5, 'A');
+	len2 = printf("Precision for character: [%*c]\n", 5, 'A');
+	printf("Length:[%d, %d]\n", len, len2);
 	return (0);
 }
